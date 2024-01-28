43a9901acaf271fc1841c667491695e4db931799
RafaelFaller <fallerrm1@hotmail.com>
2024-01-28 13:57:33 -0300

    first commit

---
+++ java/main.js
@@ -0,0 +1,10 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed:300,
+    slidesToShow: 1,
+    autoplay: true,
+    adaptiveHeight: true,
+    autoplaySpeed: 2000
+
+});
